28db23489fb641e71dd583555cf185663536e1df
bardenha <bardenha@gmail.com>
2022-01-29 20:46:42 +0000

    config: storybook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,11 @@
 import type { NextPage } from 'next'
 
-import Button from '@/components/buttons/Button'
 import WorkInProgress from '@/components/WorkInProgress'
 
 const Home: NextPage = () => {
   return (
     <>
       <WorkInProgress />
-      <Button variant="primary">Hello</Button>
     </>
   )
 }
