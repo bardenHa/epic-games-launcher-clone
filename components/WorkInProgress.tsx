c76939209f81d5c387de89b90abc2867567d19e5
bardenha <bardenha@gmail.com>
2022-01-29 14:49:27 +0000

    init commit

---
+++ components/WorkInProgress.tsx
@@ -0,0 +1,11 @@
+const WorkInProgress: React.FunctionComponent = () => {
+  return (
+    <main className="w-full flex justify-center layout items-center min-h-screen bg-red-500">
+      <h1 className="text-3xl font-mono md:text-5xl lg:text-7xl font-semibold text-white">
+        Work in progress 🚧
+      </h1>
+    </main>
+  )
+}
+
+export default WorkInProgress
