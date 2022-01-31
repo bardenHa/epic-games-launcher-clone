3fb1e35f5085d7642a526734cd02e37b0048c18d
bardenha <bardenha@gmail.com>
2022-01-31 18:27:30 +0000

    feat: framer motion animation on game buttons

--- src/components/buttons/NavigationButton.tsx
+++ src/components/buttons/NavigationButton.tsx
@@ -1,5 +1,7 @@
+import { motion } from 'framer-motion'
 import Image from 'next/image'
 import React from 'react'
+import { BiPlay } from 'react-icons/bi'
 
 import clsxm from '@/lib/clsxm'
 
@@ -31,6 +33,30 @@ interface ButtonWithGame extends Button {
 type ButtonProps = (ButtonWithIcon | ButtonWithGame) &
   React.ComponentPropsWithRef<'button'>
 
+const launchText = {
+  hover: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.15, type: 'tween', ease: 'easeInOut' },
+  },
+  rest: {
+    opacity: 0,
+    y: 10,
+    transition: { duration: 0.15, type: 'tween', ease: 'easeInOut' },
+  },
+}
+
+const childrenText = {
+  hover: {
+    y: 0,
+    transition: { duration: 0.15, type: 'tween', ease: 'easeInOut' },
+  },
+  rest: {
+    y: 'calc(50%)',
+    transition: { duration: 0.15, type: 'tween', ease: 'easeInOut' },
+  },
+}
+
 const NavigationButton = React.forwardRef<HTMLButtonElement, ButtonProps>(
   (
     {
@@ -46,57 +72,81 @@ const NavigationButton = React.forwardRef<HTMLButtonElement, ButtonProps>(
     ref
   ) => {
     return (
-      <button
-        ref={ref}
-        type="button"
-        className={clsxm(
-          'inline-flex h-14 xl:h-16 2xl:h-20 text-xs xl:text-sm 2xl:text-base items-center duration-200 transition-colors rounded-xl px-5 font-semibold text-left',
-          [
-            variant === 'primary' && [
-              'bg-primary-700 text-primary-200',
-              'border border-primary-700',
-              'hover:bg-primary-600',
-              'focus:border-primary-150',
-              'disabled:bg-primary-400 disabled:hover:bg-primary-400',
-            ],
-          ],
-          active && ['bg-primary-800 text-primary-150', 'hover:bg-primary-500'],
-          game && [
-            'pl-2.5 2xl:pl-3 h-16 2xl:h-20 text-primary-150 font-normal',
-          ],
-          className
-        )}
-        {...rest}
+      <motion.div
+        initial="rest"
+        whileHover="hover"
+        animate="rest"
+        className="w-full"
       >
-        {icon && (
-          <div className="mr-4 text-lg xl:text-xl xl:mr-5 2xl:mr-7 fill-primary-200">
-            {icon}
-          </div>
-        )}
-        {game && (
-          <div className="mr-3.5 overflow-hidden rounded-md w-7 md:w-8 xl:w-9 2xl:w-10 xl:mr-4 2xl:mr-5">
-            <Image
-              src={game}
-              alt="Quick launch game image"
-              layout="responsive"
-            />
-          </div>
-        )}
-        {isTimed && (
-          <div
-            className={clsxm(
-              'absolute h-4 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2',
-              {
-                'text-white': ['primary', 'dark'].includes(variant),
-                'text-black': ['light'].includes(variant),
-                'text-primary-500': ['outline', 'ghost'].includes(variant),
-              }
-            )}
-          ></div>
-        )}
-
-        {children}
-      </button>
+        <button
+          ref={ref}
+          type="button"
+          className={clsxm(
+            'inline-flex h-14 xl:h-16 2xl:h-20 text-xs xl:text-sm 2xl:text-base items-center duration-200 transition-colors rounded-xl px-5 font-semibold text-left w-full',
+            [
+              variant === 'primary' && [
+                'bg-primary-700 text-primary-200',
+                'border border-primary-700',
+                'hover:bg-primary-600',
+                'focus:border-primary-150',
+                'disabled:bg-primary-400 disabled:hover:bg-primary-400',
+              ],
+            ],
+            active && [
+              'bg-primary-800 text-primary-150',
+              'hover:bg-primary-500',
+            ],
+            game && [
+              'pl-2.5 2xl:pl-3 h-16 2xl:h-20 text-primary-150 font-normal',
+            ],
+            className
+          )}
+          {...rest}
+        >
+          {icon && (
+            <div className="mr-4 text-lg xl:text-xl xl:mr-5 2xl:mr-7 fill-primary-200">
+              {icon}
+            </div>
+          )}
+          {game && (
+            <div className="mr-3.5 overflow-hidden rounded-md w-7 md:w-8 xl:w-9 2xl:w-10 xl:mr-4 2xl:mr-5">
+              <Image
+                src={game}
+                alt="Quick launch game image"
+                layout="responsive"
+              />
+            </div>
+          )}
+          {isTimed && (
+            <div
+              className={clsxm(
+                'absolute h-4 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2',
+                {
+                  'text-white': ['primary', 'dark'].includes(variant),
+                  'text-black': ['light'].includes(variant),
+                  'text-primary-500': ['outline', 'ghost'].includes(variant),
+                }
+              )}
+            ></div>
+          )}
+          {game ? (
+            <div>
+              <motion.div variants={childrenText} className="mt-1 xl:mt-0">
+                {children}
+              </motion.div>
+              <motion.span
+                variants={launchText}
+                className="inline-flex items-center mt-1 2xl:text-sm xl:text-xs text-[0.6rem] text-primary-200"
+              >
+                <BiPlay className="text-lg 2xl:text-xl" />
+                Launch
+              </motion.span>
+            </div>
+          ) : (
+            children
+          )}
+        </button>
+      </motion.div>
     )
   }
 )
