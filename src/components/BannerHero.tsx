1a3618998fea76c6a3ec0f645564f25504cc67cf
bardenha <bardenha@gmail.com>
2022-02-08 21:40:36 +0000

    refactor: featured games

--- src/components/BannerHero.tsx
+++ src/components/BannerHero.tsx
@@ -3,11 +3,19 @@ import Image from 'next/image'
 import HeroButton from './buttons/HeroButton'
 
 import DylingLight2 from '~/images/banners/dyinglight2.jpg'
-import RocketLeague from '~/images/rocketleague.jpg'
 
-const BannerHero: React.FunctionComponent = () => {
+interface featuredGame {
+  title: string
+  image: StaticImageData
+}
+
+interface BannerHero {
+  featuredGames: Array<featuredGame>
+}
+
+const BannerHero: React.FunctionComponent<BannerHero> = ({ featuredGames }) => {
   return (
-    <section className="flex space-x-4 2xl:space-x-8 h-min">
+    <section className="flex lg:space-x-4 2xl:space-x-8 h-min">
       <div className="flex-auto overflow-hidden cursor-pointer h-min rounded-2xl">
         <Image
           src={DylingLight2}
@@ -16,24 +24,15 @@ const BannerHero: React.FunctionComponent = () => {
         />
       </div>
       <div className="flex-col flex-none hidden space-y-1 lg:flex">
-        <HeroButton game={RocketLeague} className="w-52 xl:w-60 2xl:w-72 h-1/6">
-          Lunar New Year Sales 2022
-        </HeroButton>
-        <HeroButton game={RocketLeague} className="w-52 xl:w-60 2xl:w-72 h-1/6">
-          Get Your Coupon
-        </HeroButton>
-        <HeroButton game={RocketLeague} className="w-52 xl:w-60 2xl:w-72 h-1/6">
-          Dying Light 2 Stay Human
-        </HeroButton>
-        <HeroButton game={RocketLeague} className="w-52 xl:w-60 2xl:w-72 h-1/6">
-          Sifu - EGS Exclusive
-        </HeroButton>
-        <HeroButton game={RocketLeague} className="w-52 xl:w-60 2xl:w-72 h-1/6">
-          Evil Dead: The Game
-        </HeroButton>
-        <HeroButton game={RocketLeague} className="w-52 xl:w-60 2xl:w-72 h-1/6">
-          Total War: WARHAMMER III
-        </HeroButton>
+        {featuredGames.map((game, index) => (
+          <HeroButton
+            key={index}
+            game={game.image}
+            className="w-52 xl:w-60 2xl:w-72 h-1/6"
+          >
+            {game.title}
+          </HeroButton>
+        ))}
       </div>
     </section>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,39 @@ import type { NextPage } from 'next'
 
 import BannerHero from '@/components/BannerHero'
 
+import RocketLeague from '~/images/rocketleague.jpg'
+
+const featuredGames = [
+  {
+    title: 'Lunar New Year Sales 2022',
+    image: RocketLeague,
+  },
+  {
+    title: 'Get Your Coupon',
+    image: RocketLeague,
+  },
+  {
+    title: 'Dying Light 2 Stay Human',
+    image: RocketLeague,
+  },
+  {
+    title: 'Sifu - EGS Exclusive',
+    image: RocketLeague,
+  },
+  {
+    title: 'Evil Dead: The Game',
+    image: RocketLeague,
+  },
+  {
+    title: 'Total War: WARHAMMER III',
+    image: RocketLeague,
+  },
+]
+
 const Store: NextPage = () => {
   return (
     <main className="w-full space-y-8 bg-primary-700 min-h-main">
-      <BannerHero />
+      <BannerHero featuredGames={featuredGames} />
       <h1 className="text-3xl font-semibold md:text-5xl lg:text-7xl">Store</h1>
     </main>
   )
